0dfa593c5795ff9a9953dc8deacaae128140e786
amani <amanii1994@outlook.com>
2023-06-20 03:22:38 +0300

    add coach + add tournament & delete coach + tourna

--- Alkhoud/main_app/urls.py
+++ Alkhoud/main_app/urls.py
@@ -40,22 +40,23 @@ urlpatterns = [
     path('Subscribers/add-subscriber/', views.add_subscriber, name='add_subscriber'),
     path('payment/', views.payment_page, name='payment_page'),
 
-
-    path('Clubs/<club_id>/coaches/add/', views.add_coach, name='add_coach'),
-
-
-
-
-   
-
     path('Clubs/club_details/<club_id>', views.club_details, name='club_details'),
     path('buy/', views.buy, name='buy'),
 
+    path('add_package/<club_id>/', views.add_package, name='add_package'),
+    path('add_offer/<club_id>/', views.add_offer, name='add_offer'),
+    path('add_coach/<club_id>/', views.add_coach, name='add_coach'),
+    #path('add_tournament/<club_id>/', views.add_tournament, name='add_tournament'),
 
-    path('add_package/<club_id>', views.add_package, name='add_package'),
-    path('add_offer/<club_id>', views.add_offer, name='add_offer'),
+
     path("Clubs/<club_id>/Packages/delete/<pack_id>/", views.delete_package, name="delete_package"),
     path("Clubs/<club_id>/Offers/delete/<offer_id>/", views.delete_offer, name="delete_offer"),
+    path("Clubs/<club_id>/Coaches/delete/<coach_id>/", views.delete_coach, name="delete_coach"),
+    #path("Clubs/<club_id>/Tournaments/delete/<tournament_id>/", views.delete_tournament, name="delete_tournament"),
+
+
+
+
 
 
 ]
